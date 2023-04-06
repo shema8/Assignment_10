42c6dee4192bf68d40e3d23c4032da267c32df9d
agwaBom <khyunjin1993@gmail.com>
2023-04-06 16:38:41 +0900

    train_file_list_to_json implemented

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
         processed_file_list.append(template_start + english_file + template_mid + german_file + template_end)
     return processed_file_list
 
-
 def write_file_list(file_list: List[str], path: str) -> None:
     """Writes a list of strings to a file, each string on a new line"""
     with open(path, 'w') as f:
